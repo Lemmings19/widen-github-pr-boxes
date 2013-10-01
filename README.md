f0af58e712b641b7893a15a1132800324f763d1a
joe lynch <jlynch@blacklinegps.com>
2013-10-01 12:49:38 -0600

    Added script, updated readme.

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ widen-github-pr-boxes
 =====================
 
 Widens Github's pull request code boxes for easier reviewing without having to horizontally scroll.
+
+Install using GreaseMonkey or TamperMonkey.
---
+++ WidenGithubPRBoxes.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @id           org.userscripts.users.lemmings19.widengithubprboxes
+// @name         Widen Github PR Boxes
+// @version      0.1
+// @author       Lemmings19
+// @description  I'm tired of code reviewing in cramped little boxes.
+// @match        https://github.com/*/pull/*
+// ==/UserScript==
+
+var container = document.getElementsByClassName("container");
+container[2].setAttribute("style", "margin-left:5% ; margin-right:5%");
+container[2].setAttribute("class", "none");
+
+var repositoryWithSidebar = document.getElementsByClassName("repository-with-sidebar");
+repositoryWithSidebar[0].setAttribute("style", "width:100%");
+
+var contextLoaderContainer = document.getElementsByClassName("context-loader-container");
+contextLoaderContainer[0].setAttribute("style", "width:95%");
